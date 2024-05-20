3f55e70a48196153a2d1991c71082aebd13a527b
benschop-it <65778673+benschop-it@users.noreply.github.com>
2024-05-20 16:39:11 +0200

    Utilities.Manifest NET 8

--- Utilities/Manifest/Properties/AssemblyInfo.cs
+++ Utilities/Manifest/Properties/AssemblyInfo.cs
@@ -27,16 +27,6 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: ClearCanvas.Common.Plugin]
-
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("ClearCanvas Utilities.Manifest")]
-[assembly: AssemblyDescription("ClearCanvas.Utilities.Manifest")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("ClearCanvas Inc.")]
-[assembly: AssemblyProduct("Utilities")]
-[assembly: AssemblyCopyright("Copyright (c) 2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -47,16 +37,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("e94613a5-9524-4f63-b6c7-ab7c6efba61a")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
